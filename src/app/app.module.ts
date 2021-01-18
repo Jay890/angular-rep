8021068a0b4b69c4c64af0db66f08270e115bd0c
Jason Leong <42172421+Jay890@users.noreply.github.com>
2021-01-18 11:37:42 +1000

    Registering the component
    Registering component to angular
    Importing the path for the typescript
    Using the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,11 @@ import { AppComponent } from "./app.component";
 // import is typescript feature
 import { FormsModule } from "@angular/forms";
 
+// import the ServerComponent
+import { ServerComponent } from "./server/server.component";
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
