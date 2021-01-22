16282c2b1aa84a30a5183360b67d04d67fb7a206
Jason Leong <42172421+Jay890@users.noreply.github.com>
2021-01-22 15:49:35 +1000

    String interpolation and property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,11 @@ import { Component } from "@angular/core";
   selector: "app-server",
   templateUrl: "./server.component.html",
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId = 1;
+  serverStatus = "offline";
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
